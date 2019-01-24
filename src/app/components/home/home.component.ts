6213e75f501414d54d42cd9051713c76a720eae0
bruno aguilar <josebrunoaguilar@gmail.com>
2019-01-24 00:51:05 -0400

    Primer commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../../services/youtube.service'
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: []
+})
+export class HomeComponent implements OnInit {
+  videos:any[]=[];
+  constructor(public _yts:YoutubeService) { 
+      this._yts.getVideos().subscribe(videos=>{
+          console.log(videos);
+          this.videos = videos;
+      });
+  }
+
+  ngOnInit() {
+  }
+
+}
